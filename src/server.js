abde5eae8918844ad877ad98b91730849506aaa2
Remi COMBELLES <recombelle1@iut.local>
2018-11-30 09:36:10 +0100

    Deployement en ligne

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express()
-const models = require('./models/index');
+//const models = require('./models/index');
 
 // Decode json and x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,6 +13,10 @@ app.use(bodyParser.json())
 // Add a bit of logging
 app.use(morgan('short'))
 
+app.get('/',function (req,res){
+	res.send('Hello Word !')
+})
+/*
 // Get all the users defined
 app.get('/', function (req, res) {
   models.User.findAll()
@@ -21,7 +25,10 @@ app.get('/', function (req, res) {
     })
 })
 
+
+
 // Add a new user to the database
+
 app.post('/', function(req, res) {
   models.User.create({
     username: req.body.username
@@ -29,16 +36,20 @@ app.post('/', function(req, res) {
     .then(() => {
       res.send('User added !')
     })
-})
+})*/
+
+//process.env.ENABLE_METRICS
 
 // Synchronize models
+
+/*
 models.sequelize.sync().then(function() {
-  /**
+   *
    * Listen on provided port, on all network interfaces.
    * 
    * Listen only when database connection is sucessfull
-   */
-  app.listen(3000, function() {
-    console.log('Express server listening on port 3000');
+   **/
+  app.listen(8080, function() {
+    console.log('Express server listening on port 8080');
   });
-});
+//});
